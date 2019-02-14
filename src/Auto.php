87f0931492203a331829bd1b93fc417e4f22c10f
Maike Alame <maike.alame@gmail.com>
2019-02-14 14:21:26 -0200

    Update Auto.php

--- src/Auto.php
+++ src/Auto.php
@@ -1,34 +1,183 @@
 <?php
-namespace PhpAutoWhere;
 
-include('DB.php');
-include('Where.php');
+namespace Auto;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Request;
+use Auto\Contracts\AutoInterface;
 use PhpAutoWhere\Where;
-use PhpAutoWhere\DB;
 
-class Auto
+class Auto implements AutoInterface
 {
     public $_core = "laravel";
     public $_class;
-    public $_config = __auto_config;
+    public $_config;
     public $_db;
     public $_dbtype;
+    public $having = [];
+    public $fields = [];
 
-    // instance for chain methods
-    public $_instance = null;
+    /**
+     * Create a new AutoWhere instance.
+     */
+    public function __construct(){
+        $this->_db = new AutoDB();
+        $this->_config = (object) Config::get("laravelauto");
+        $this->_dbtype = $this->_config->db["type"];
+    }
+
+
+
+
+    public function setPdo($pdo){
+        $this->_db = new AutoDB($pdo);
+        return $this->getInstance();
+    }
+
+    /**
+     * Initialize module Where
+     */
+    public function where(){
+        $this->_class = new Where($this);
+//        dd($this->_class);
+        return $this->getInstance();
+    }
+
+    /**
+     * Set table by class
+     *
+     * @param $class
+     *
+     * @return Auto
+     */
+    public function setClass($class){
+        $this->_class->table( (new $class)->getConnection()->getDatabaseName() . "." . (new $class)->getTable() );
+        return $this->getInstance();
+    }
+
+    /**
+     * get quickly where sql without params
+     *
+     * @return string
+     */
+    public function get(){
+        $where = Request::get('filter') ? Request::get('filter') : [];
+        foreach($this->fields as $k=>$v){
+            $where[$k] = $v;
+        }
+        $having = Request::get('filter_having') ? Request::get('filter_having') : [];
+        foreach($having as $k=>$v){ unset($where[$k]); }
+        $columns = Request::get('columns') ? Request::get('columns') : [];
+        return $this->_class->columns($columns)->render($where);
+    }
+
+    /**
+     * get quickly having sql without params
+     *
+     * @return string
+     */
+    public function getHaving(){
+        $where = Request::get('filter') ? Request::get('filter') : [];
+        foreach($this->fields as $k=>$v){
+            $where[$k] = $v;
+        }
+        $hav = Request::get('filter_having') ? Request::get('filter_having') : [];
+        $having = array_filter($where,function($key) use($hav){
+            return in_array($key,array_keys($hav));
+        },ARRAY_FILTER_USE_KEY);
+        $r = $this->_class->columns($hav)->render($having);
+        return  trim($r) == "true" ? "true" : $r;
+    }
 
 
     /**
-     * Constructor
+     * set default field value for one column in the query
+     *
+     * @return void
      */
-    public function __construct(){
-        $this->_config = (object) $this->_config;
-        $this->_db = DB::getConnection($this->_config->db);
-        $this->_dbtype = $this->_config->db['type'];
-
-        if ($this->_instance === null) $this->_instance = $this;
+    public function setDefaultField($column, $value){
+        $filter = Request::get("filter");
+        if(!$filter) $filter = [];
+        isset($filter[$column]) ?: Request::merge(["filter"=>array_merge( $filter, [$column=>$value] )]);
     }
+
+    /**
+     * set field value for one column in the query
+     *
+     * @return void
+     */
+    public function setField($column, $value){
+//        Request::has("filter") ?: Request::merge(["filter"=>[]]);
+//        Request::merge(["filter"=>array_merge(Request::get("filter"),[$column=>$value])]);
+        $this->fields = array_merge($this->fields,[$column=>$value]);
+    }
+
+    /**
+     * set column type in the query
+     *
+     * @return void
+     */
+    public function setColumn($column, $type){
+        Request::has("columns") ?: Request::merge(["columns"=>[]]);
+        Request::merge(["columns"=>array_merge(Request::get("columns"),[$column=>$type])]);
+    }
+
+    /**
+     * set withTrashed soft delete
+     *
+     * @return void
+     */
+    public function withTrashed(){
+        Request::merge(["trashed"=> Request::has("trashed") ? Request::get("trashed") : 1]);
+    }
+
+    /**
+     * set withoutTrashed soft delete
+     *
+     * @return void
+     */
+    public function withoutTrashed(){
+        Request::merge(["trashed"=> Request::has("trashed") ? Request::get("trashed") : 0]);
+    }
+
+
+    /**
+     * set onlyTrashed soft delete
+     *
+     * @return void
+     */
+    public function onlyTrashed(){
+        Request::merge(["trashed"=> Request::has("trashed") ? Request::get("trashed") : 2]);
+    }
+
+
+
+    /**
+     * set ignore filter
+     *
+     * @return void
+     */
+    public function ignore($column){
+        Request::has("filter_ignore") ?: Request::merge(["filter_ignore"=>[]]);
+        Request::merge(["filter_ignore"=>array_push(Request::get("filter_ignore"),$column)]);
+    }
+
+
+    /**
+     * set having filter, excludes from 'where' on query
+     *
+     * @return void
+     */
+    public function having($column, $type = null){
+        Request::has("filter_having") ?: Request::merge(["filter_having"=>[]]);
+        Request::merge(["filter_having"=>
+            is_array($column) ?
+                array_merge(Request::get("filter_having"),$column) :
+                array_push(Request::get("filter_having"),[$column=>$type])
+        ]);
+    }
+
 
     /**
      * Generate chain methods
@@ -36,35 +185,38 @@ class Auto
      * @return Auto
      */
     public function getInstance(){
-        if ($this->_instance === null) $this->_instance = $this;
-        return $this->_instance;
+        return $this;
     }
 
 
+    /**
+     * Call functions in $_class or $this
+     *
+     * @return mixed
+     */
     public function __call($method,$arguments) {
         if($this->_class) {
             if (method_exists($this->_class, $method)) {
+
                 return call_user_func_array(array($this->_class, $method), $arguments);
+
             }elseif( $method == "or"){ // Keyword reserved of php
 
                 return call_user_func_array(array($this->_class, "_or"), $arguments);
 
-            }else return $this;
-        }else{
+            }
+        }
+        if( $method == "class"){ // Keyword reserved of php
+
+            return call_user_func_array(array($this, "setCclass"), $arguments);
+
+        }
+
+        if (method_exists($this, $method)) {
             return call_user_func_array(array($this, $method), $arguments);
         }
-    }
+        return $this->getInstance();
 
-    /**
-     * Initialize Where module
-     *
-     * @return Auto
-     */
-    public static function where(){
-        $auto = new self();
-        $auto->_class = new Where($auto);
-        return $auto->getInstance();
     }
 
 }
-?>
